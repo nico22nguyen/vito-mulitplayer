812bf4d802873aa3641cef7b48338fb5875dd35d
Nic Nguyen <nico.nguyen22@gmail.com>
2024-03-20 21:52:39 -0500

    debug stuff

--- obj/Debug/net8.0-windows/Test.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8407373aa8efcf869cb9d4fc6fc1d67e8009000")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
