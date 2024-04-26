2285fd12d3e4f4b246a55ad480d4e4e3bf7eb7b1
Nic Nguyen <nico.nguyen22@gmail.com>
2024-04-25 22:51:48 -0500

    removed comments

--- Program.cs
+++ Program.cs
@@ -9,19 +9,12 @@ static class Program
     [STAThread]
     static void Main(string[] args)
     {
-		/*
-		if (args.Length == 0) {
-			Console.WriteLine("Missing argument. Either specify -host or provide the IP of the host to join");
-			return;
-		}
-		*/
-		bool host = false;
+		
+		bool host = false; // switch to true to host
 		IPAddress otherIP;
-		if (host) {//args[0].Equals("-host")) {
-			host = true;
+		if (host) {
 			otherIP = IPAddress.Any;
 		} else {
-			host = false;
 			otherIP = IPAddress.Parse("127.0.0.1");
 		}
         // To customize application configuration such as set high DPI settings or default font,
